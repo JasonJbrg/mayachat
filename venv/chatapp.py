dcabebc5f679fb921be2eae06d73bd36cc41e5c6
JasonJbrg <138401176+JasonJbrg@users.noreply.github.com>
2023-07-13 10:39:23 -0700

    Update chatapp.py

--- venv/chatapp.py
+++ venv/chatapp.py
@@ -12,17 +12,16 @@ import io
 from docx import Document
 from datetime import datetime
 import random
-import language_tool_python
 
 
 
 # Load environment variables from .env file
-load_dotenv()
-
+dotenv_path = "PycharmProjects/.env"
+load_dotenv(dotenv_path)
 
 
 # Read the config.json file
-with open("/Users/jasons/PycharmProjects/pythonProject/venv/config.json") as file:
+with open("venv/config.json") as file:
     config = json.load(file)
 
 # Extract the values from the config dictionary
@@ -37,8 +36,9 @@ greetings = config["greetings"]
 load_dotenv('/Users/jasons/PycharmProjects/pythonProject/PycharmProjects/.env')
   # take environment variables from .env.
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
-api_key = os.getenv('OPENAI_API_KEY')
+# Set OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 # Set page configuration
 st.set_page_config(
@@ -160,6 +160,12 @@ else:
     user_prompt = ''
 btn_enter = st.button("Enter")
 
+MAX_TOKENS = 500
+MAX_TOKENS_PER_MESSAGE = 50
+# Prepare the conversation for the chat model
+conversation = [
+      {"role": "assistant", "content": initial_context[st.session_state.selected_task]},
+] + st.session_state.hst_chat
 
 # When 'Enter' button is clicked
 if btn_enter and user_prompt:
@@ -203,12 +209,7 @@ if btn_enter and user_prompt:
     # ...
     # ...
 
-    MAX_TOKENS = 500
-    MAX_TOKENS_PER_MESSAGE = 50
-    # Prepare the conversation for the chat model
-    conversation = [
-                       {"role": "assistant", "content": initial_context[st.session_state.selected_task]},
-                   ] + st.session_state.hst_chat
+
 
     # Calculate the total number of tokens in the conversation
     total_tokens = sum(len(message['content'].split()) for message in conversation)
