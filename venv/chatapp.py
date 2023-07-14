71b0940048687a396dd5ecbdae2dae0a6da0e655
JasonJbrg <138401176+JasonJbrg@users.noreply.github.com>
2023-07-14 15:38:24 -0700

    Update chatapp.py

--- venv/chatapp.py
+++ venv/chatapp.py
@@ -95,10 +95,11 @@ selected_language = 'Select...'
 selected_task = 'Select...'
 
 # Create containers
-container1 = st.container()
-container2 = st.container()
-container3 = st.container()
-container4 = st.container()
+def main ():
+    container1 = st.container()
+    container2 = st.container()
+    container3 = st.container()
+    container4 = st.container(align_bottom = True)
 
 with container1:
     pass  # Container 1 is empty
