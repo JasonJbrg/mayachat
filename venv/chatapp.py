adeba208e4c6bc7d6eb93a076020be27b652f3ae
JasonJbrg <138401176+JasonJbrg@users.noreply.github.com>
2023-07-17 20:17:28 -0700

    Update chatapp.py

--- venv/chatapp.py
+++ venv/chatapp.py
@@ -79,7 +79,7 @@ languages.update({
 
 # Define default values for selected_language and selected_task
 selected_language = 'Select Language'
-selected_task = 'Select...'
+selected_task = 'Select Topic'
 
 
 # Initialize new_message and return_openai to None
@@ -87,7 +87,7 @@ new_message = None
 return_openai = None
 
 # Get user input for language selection
-selected_language = st.selectbox("Select your language", list(languages.keys()), key='language_selection')
+selected_language = st.selectbox("", list(languages.keys()), key='language_selection')
 
 if selected_language != 'Select Language':
     # Initialize the Translator with the selected language
@@ -98,13 +98,13 @@ if selected_language != 'Select Language':
     translator_from_en = Translator(from_lang="en", to_lang=languages[selected_language])
 
     # Add a default option to the task_selection list
-    task_selection = ['Select...'] + task_selection
+    task_selection = ['Select Topic'] + task_selection
 
     # Get user input for task selection
     selected_task = st.selectbox("Select a task", task_selection, key='task_selection')
 
     # Only update the selected task in session state if a task is selected
-    if selected_task != 'Select...':
+    if selected_task != 'Select Topic':
         st.session_state.selected_task = selected_task
 
         # Only proceed if a task is selected and the chat history is empty
@@ -159,7 +159,7 @@ if 'selected_task' in st.session_state:
 
 
 # Add a default option to the task_selection list
-task_selection = ['Select...'] + task_selection
+task_selection = ['Select Topic'] + task_selection
 
 # Initialize chat history in session state if not already present
 if 'hst_chat' not in st.session_state:
@@ -168,7 +168,7 @@ if 'hst_chat_time' not in st.session_state:
     st.session_state.hst_chat_time = []
 
 # Only proceed if a task is selected and the chat history is empty
-if selected_task != 'Select...' and not st.session_state.hst_chat:
+if selected_task != 'Select Topic' and not st.session_state.hst_chat:
     # Update the selected task in session state
     st.session_state.selected_task = selected_task
     # Choose a random greeting for the selected task
